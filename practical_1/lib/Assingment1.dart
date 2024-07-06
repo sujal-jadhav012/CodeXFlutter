4f32c0ed84a7025d877840c133bf9cd2c2cf7a31
sujal_jadhav <sujaljadhav0011@gmail.com>
2024-07-06 14:07:19 +0530

    Assingment 1 done

---
+++ practical_1/lib/Assingment1.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Assingment1 extends StatelessWidget {
+  const Assingment1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      backgroundColor: Color.fromARGB(20, 213, 169, 39),
+        title:Text("MY App") ,
+
+      ),
+    );
+  }
+}
